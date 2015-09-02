7e0b8e2c32bc7a59a7e5fcca4b774ed16fe05ebb
bing.liu <mobi_neoliu@163.com>
2015-09-02 14:41:08 +0800

    init

---
+++ RoomMate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RoomMate
+//
+//  Created by bingliu on 15/9/2.
+//  Copyright (c) 2015年 GoodCraft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
